6c6432d6113b61c678d1a4a15adea29d1f5f82bf
Andrei Dumitrescu <andumywow@gmail.com>
2020-07-19 13:52:27 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curs 1 - Live</title>
+</head>
+<body>
+    <h1>My first web-page via Docker!!</h1>
+</body>
+</html>
